739f16bdd439090883a5ce53bccf20508779c630
OgiIlic <ognjen.ilic.22@singimail.rs>
2025-03-08 18:16:05 +0100

    Added cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 
+
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
